db149a6e2b9d458164ca2d003136a40e9368facf
Lucas Moraes Campos <lukaspgtu14@gmail.com>
2019-09-27 19:47:36 -0300

    up

--- src/app/pages/system/system.component.ts
+++ src/app/pages/system/system.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, TemplateRef, OnInit } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-system',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemComponent implements OnInit {
 
-  constructor() { }
+  modalRef: BsModalRef;
+  constructor(private modalService: BsModalService) {}
 
   ngOnInit() {
+  }
+
+  openModal(template: TemplateRef<any>, length: string = 'modal-lg') {
+    this.modalRef = this.modalService.show(template, {class: `modal-dialog-centered ${length}`, ignoreBackdropClick: true});
   }
 
 }
